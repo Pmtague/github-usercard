7a88cd33c7af13224e9669edf6bafaf865627187
Penny Tague <pmtague9@gmail.com>
2019-07-11 15:56:51 -0700

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,11 +4,13 @@
 */
 
 axios.get('https://api.github.com/users/Pmtague')
-  .then( response => {
-    console.log('response', response)
+  .then(data => {
+    const cards = document.querySelector('.cards');
+    const card = createCard(data.data);
+    cards.appendChild(card);
   })
   .catch( error => {
-    console.log('Error:', error)
+    console.log('Get your life together:', error);
   })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -32,8 +34,28 @@ axios.get('https://api.github.com/users/Pmtague')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+];
 
+followersArray.forEach(item => {
+  axios.get(`https://api.github.com/users/${item}`)
+  .then(data => {
+    console.log('data', data);
+    const cards = document.querySelector('.cards');
+    const card = createCard(data.data);
+    cards.appendChild(card);
+    console.log(cards);
+    
+  })
+  .catch( error => {
+    console.log('Get your life together:', error);
+  })
+});
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -62,12 +84,10 @@ const followersArray = [];
   bigknell
 */
 
-const cards = document.querySelector('.cards');
-
 
 
 // Create and Return DOM Node
-function createCard(object) {
+function createCard(data) {
   
   // Create Elements
   const card = document.createElement('div');
@@ -101,10 +121,15 @@ function createCard(object) {
   username.classList.add('username');
 
   // Set Content
-
-
-  // Put Together (appendChild)
-
+  image.src = `${data.avatar_url}`;
+  name.textContent = `Name: ${data.name}`;
+  username.textContent = `Username: ${data.login}`;
+  location.textContent = `Location: ${data.location}`;
+  link.href = data.html_url;
+  link.textContent = `Profile: ${data.html_url}`;
+  followers.textContent = `Followers: ${data.followers}`;
+  following.textContent = `Following: ${data.following}`;
+  bio.textContent = `Bio: ${data.bio}`;
 
   return card
 }
